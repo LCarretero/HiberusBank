7071273d04391da12adc20423d6e396c53dc8ea4
Lisardo Carretero Colmenar <lcarretero@hiberus.com>
2023-09-22 14:56:38 +0200

    mockito test update

--- demo/src/test/java/com/example/demo/WorkerServiceTest.java
+++ demo/src/test/java/com/example/demo/WorkerServiceTest.java
@@ -2,44 +2,69 @@ package com.example.demo;
 
 
 import com.example.demo.dto.WorkerDTO;
+import com.example.demo.entities.Payroll;
+import com.example.demo.entities.Transfer;
 import com.example.demo.entities.Worker;
 import com.example.demo.exceptions.workerExceptions.WorkerBadRequestException;
 import com.example.demo.exceptions.workerExceptions.WorkerConflictException;
 import com.example.demo.exceptions.workerExceptions.WorkerNotFoundException;
+import com.example.demo.repositories.PayrollRepository;
+import com.example.demo.repositories.TransferRepository;
 import com.example.demo.repositories.WorkerRepository;
 import com.example.demo.services.implementation.WorkerServiceImp;
 import com.example.demo.services.interfaces.WorkerService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class WorkerServiceTest {
 
-    @Mock
-    private WorkerService workerService;
+    @InjectMocks
+    private WorkerServiceImp workerService;
 
     @Mock
     private WorkerRepository workerRepository;
+    @Mock
+    private PayrollRepository payrollRepository;
+    @Mock
+    private TransferRepository transferRepository;
 
     private Worker workerTest;
 
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.openMocks(this);
-        workerService = new WorkerServiceImp();
         workerTest = new Worker(UUID.randomUUID(), "77991221V", "Lisardo", "Carretero", 6000, 0.0, new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+        workerTest.getPayrolls().add(1L);
+        workerTest.getTransfersReceived().add(1L);
+        workerTest.getTransfersEmitted().add(1L);
     }
 
+    @Test
+    public void testConflictInSaveWorker() throws WorkerConflictException, WorkerBadRequestException {
+        when(workerRepository.findByDni(any())).thenReturn(workerTest);
+        assertThrows(WorkerConflictException.class, () -> workerService.saveWorker(workerTest));
+    }
+
+    @Test
+    public void testWorkerBadRequestExceptionInSaveWorker() throws WorkerConflictException, WorkerBadRequestException {
+        Worker workerWithBadDNI = workerTest = new Worker(UUID.randomUUID(), "7991221V", "Lisardo", "Carretero", 6000, 0.0, new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+
+        assertThrows(WorkerBadRequestException.class, () -> workerService.saveWorker(workerTest));
+    }
     @Test
     public void testSaveWorker() throws WorkerConflictException, WorkerBadRequestException {
         //When
@@ -56,23 +81,32 @@ class WorkerServiceTest {
 
     @Test
     public void testDeleteWorker() {
-        workerRepository.delete(workerTest);
-        verify(workerRepository, only()).delete(workerTest);
-        verify(workerRepository, atMost(1)).delete(workerTest);
+        doNothing().when(workerRepository).delete(workerTest);
+        when(workerRepository.findByDni(any())).thenReturn(workerTest);
+        workerService.deleteWorker(workerTest.getDni());
+    }
+    @Test
+    public void testDeleteWorkerOnCascade() {
+        doNothing().when(workerRepository).delete(workerTest);
+        doNothing().when(payrollRepository).deleteById(any());
+        doNothing().when(transferRepository).deleteById(any());
+        when(workerRepository.findByDni(any())).thenReturn(workerTest);
+
+        workerService.deleteWorker(workerTest.getDni());
     }
 
     @Test
     public void testWorkerInformation() throws WorkerConflictException, WorkerBadRequestException, WorkerNotFoundException {
-        //Given
-        workerService.saveWorker(workerTest);
 
         //When
-        when(workerRepository.getReferenceById(workerTest.getId())).thenReturn(workerTest);
+        when(workerRepository.findByDni(any())).thenReturn(workerTest);
+
+        //Given
 
         WorkerDTO result = workerService.workerInformation(workerTest.getDni());
 
         //Then
-        verify(workerRepository, times(1)).getReferenceById(workerTest.getId());
+        verify(workerRepository, times(1)).findByDni(any());
 
         assertEquals("77991221V", result.getDni());
         assertEquals("Lisardo", result.getName());
